107b8ae450f7f6d1c8a9497c752e95528288da0d
Kazu Pang <72171546+Kazu-Pang@users.noreply.github.com>
2026-04-07 15:40:06 -0700

    added tests for team tostring, equals, and hashcode

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,9 +1,13 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 public class TeamTest {
 
@@ -22,5 +26,50 @@ public class TeamTest {
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
 
+    @Test
+    public void equals_with_same_object() {
+        assertTrue(team.equals(team), "same object.equals(same object) should be true");
+    }
+
+    @Test
+    public void equals_with_different_class() {
+        String diff_class = "different class";
+        assertFalse(team.equals(diff_class), "class.equals(different class) should be false");
+    }
+
+    // May be redundant because of following two tests below
+    @Test
+    public void equals_with_same_name_and_members() {
+        Team same_name = new Team("test-team");
+        assertTrue(team.equals(same_name), "class.equals(class with same name and members) should be true");
+    }
+
+    @Test
+    public void equals_with_different_name() {
+        Team diff_name = new Team("different name");
+        assertFalse(team.equals(diff_name), "class.equals(class with diff name) should be false");
+    }
+
+    @Test
+    public void equals_with_different_members() {
+        Team diff_members = new Team("test-team");
+
+        ArrayList<String> members = new ArrayList<String>();
+        members.add("member that does not exist in original class");
+        diff_members.setMembers(members);
+
+        assertFalse(team.equals(diff_members), "class.equals(class with diff members) should be false");
+    }
+
+    @Test
+    public void hashCode_returns_expected_value() {
+        int hash = team.hashCode();
+        int expected = -1226298695; 
+        assertEquals(hash, expected);
+    }
 }
